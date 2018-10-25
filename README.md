b8cccfb0236459116ea0258efd7fa73ccd871676
abel <abelcarreras83@gmail.com>
2018-10-25 13:41:17 +0200

    Move the kwargs of the call to setup() in setup.py into a setup.json

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Build Status](https://travis-ci.org/abelcarreras/aiida-lammps.svg?branch=master)](https://travis-ci.org/abelcarreras/aiida-lammps)
 
 AiiDA LAMMPS plugin
 ====================
--- setup.py
+++ setup.py
@@ -1,43 +1,13 @@
+from __future__ import absolute_import
 from setuptools import setup, find_packages
+import json
 
-setup(
-    name='aiida-lammps',
-    version='0.1.0a1',
-    description='AiiDA plugin for LAMMPS',
-    url='https://github.com/abelcarreras/aiida_extensions',
-    author='Abel Carreras',
-    author_email='abelcarreras83@gmail.com',
-    license='MIT license',
-    packages=find_packages(),
-    install_requires=['aiida-core>=0.12.0', 'numpy', 'packaging', 'jsonschema'],
-    setup_requires=['reentry'],
-    reentry_register=True,
-    include_package_data=True,  # puts non-code files into the distribution, reads list from MANIFEST.in
-    entry_points={
-        'aiida.calculations': [
-            'lammps.combinate = aiida_lammps.calculations.lammps.combinate:CombinateCalculation',
-            'lammps.force = aiida_lammps.calculations.lammps.force:ForceCalculation',
-            'lammps.md = aiida_lammps.calculations.lammps.md:MdCalculation',
-            'lammps.optimize = aiida_lammps.calculations.lammps.optimize:OptimizeCalculation',
-            'dynaphopy = aiida_lammps.calculations.dynaphopy: DynaphopyCalculation'],
-        'aiida.parsers': [
-            'lammps.force = aiida_lammps.parsers.lammps.force:ForceParser',
-            'lammps.md = aiida_lammps.parsers.lammps.md:MdParser',
-            'lammps.optimize = aiida_lammps.parsers.lammps.optimize:OptimizeParser',
-            'dynaphopy = aiida_lammps.parsers.dynaphopy: DynaphopyParser']
-        },
-    extras_require={
-        "testing": {
-            "mock==2.0.0",
-            "pgtest==1.1.0",
-            "sqlalchemy-diff==0.1.3",
-            "pytest==3.6.3",
-            "pytest-timeout",
-            "wheel>=0.31"
-        },
-        "phonopy": {
-            'dynaphopy',
-            # 'aiida_phonopy' # needs to be added to pip
-        }
-    }
-    )
+if __name__ == '__main__':
+    with open('setup.json', 'r') as info:
+        kwargs = json.load(info)
+
+    setup(packages=find_packages(),
+          long_description=open('README.md').read(),
+          long_description_content_type='text/markdown',
+          **kwargs
+          )
