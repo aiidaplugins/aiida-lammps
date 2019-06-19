d070f70aefc2faea3efcdf6f9a1c325436ccc5c5
Chris Sewell <chrisj_sewell@hotmail.com>
2019-06-19 10:11:32 +0100

    test fixes (and add greigite structure test)

--- aiida_lammps/tests/test_generate_structure.py
+++ aiida_lammps/tests/test_generate_structure.py
@@ -1,11 +1,14 @@
 import pytest
+import six
 from aiida_lammps.common.generate_structure import generate_lammps_structure
 
 
 @pytest.mark.parametrize('structure', [
     "pyrite",
-    "fes_cubic-zincblende"
+    "fes_cubic-zincblende",
+    "greigite"
 ])
 def test_generate(db_test_app, get_structure_data, structure, file_regression):
     structure = get_structure_data(structure)
-    file_regression.check(generate_lammps_structure(structure))
+    file_regression.check(six.ensure_text(
+        generate_lammps_structure(structure, round_dp=8)))
