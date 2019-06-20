d796e29ebc4993f056b8fc8f86a258d52ffda951
Chris Sewell <chrisj_sewell@hotmail.com>
2019-06-20 13:03:22 +0100

    add reaxff energy contribution variables

--- aiida_lammps/data/potential/reaxff.py
+++ aiida_lammps/data/potential/reaxff.py
@@ -25,6 +25,21 @@ def get_input_potential_lines(data, names=None, potential_filename='potential.po
     lammps_input_text += "fix qeq all qeq/reax 1 0.0 10.0 1e-6 reax/c\n"
     lammps_input_text += "fix_modify qeq energy yes\n"
 
+    lammps_input_text += "variable reax_eb           equal c_reax[1]\n"
+    lammps_input_text += "variable reax_ea           equal c_reax[2]\n"
+    lammps_input_text += "variable reax_elp          equal c_reax[3]\n"
+    lammps_input_text += "variable reax_emol         equal c_reax[4]\n"
+    lammps_input_text += "variable reax_ev           equal c_reax[5]\n"
+    lammps_input_text += "variable reax_epen         equal c_reax[6]\n"
+    lammps_input_text += "variable reax_ecoa         equal c_reax[7]\n"
+    lammps_input_text += "variable reax_ehb          equal c_reax[8]\n"
+    lammps_input_text += "variable reax_et           equal c_reax[9]\n"
+    lammps_input_text += "variable reax_eco          equal c_reax[10]\n"
+    lammps_input_text += "variable reax_ew           equal c_reax[11]\n"
+    lammps_input_text += "variable reax_ep           equal c_reax[12]\n"
+    lammps_input_text += "variable reax_efi          equal c_reax[13]\n"
+    lammps_input_text += "variable reax_eqeq         equal c_reax[14]\n"
+
     return lammps_input_text
 
 
