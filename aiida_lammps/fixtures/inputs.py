f4b5570f8284327c576dc63e891a7ab27bcaa4cb
Jonathan Chico <jonathanpchico@gmail.com>
2023-03-26 19:23:16 +0200

    (WIP) Fixing some pre-commit errors.

--- aiida_lammps/fixtures/inputs.py
+++ aiida_lammps/fixtures/inputs.py
@@ -1,6 +1,7 @@
 """Set of data for the testing of the input generation"""
-import pytest
 from aiida.common import AttributeDict
+import pytest
+
 
 @pytest.fixture
 def restart_data() -> AttributeDict:
@@ -10,16 +11,16 @@ def restart_data() -> AttributeDict:
     :rtype: AttributeDict
     """
     data = AttributeDict()
-    
+
     data.final = (
-        '#---------------------Start of the write restart information---------------------#\n'
-        'write_restart restart.aiida\n'
-        '#----------------------End of the write restart information----------------------#\n'
+        "#---------------------Start of the write restart information---------------------#\n"
+        "write_restart restart.aiida\n"
+        "#----------------------End of the write restart information----------------------#\n"
     )
     data.intermediate = (
-        '#--------------Start of the intermediate write restart information---------------#\n'
-        'restart restart.aiida 100\n'
-        '#---------------End of the intermediate write restart information----------------#\n'
+        "#--------------Start of the intermediate write restart information---------------#\n"
+        "restart restart.aiida 100\n"
+        "#---------------End of the intermediate write restart information----------------#\n"
     )
     return data
 
@@ -33,26 +34,32 @@ def parameters_md() -> AttributeDict:
     """
     parameters = AttributeDict()
     parameters.compute = AttributeDict()
-    parameters.compute['ke/atom'] = [{'group': 'all', 'type':[{'keyword': " ", "value": " "}]}]
-    parameters.compute['pe/atom'] = [{'group': 'all', 'type':[{'keyword': " ", "value": " "}]}]
-    parameters.compute.pressure = [{'group': 'all', 'type':['thermo_temp']}]
-    parameters.compute['stress/atom'] = [{'group': 'all', 'type':['NULL']}]
+    parameters.compute["ke/atom"] = [
+        {"group": "all", "type": [{"keyword": " ", "value": " "}]}
+    ]
+    parameters.compute["pe/atom"] = [
+        {"group": "all", "type": [{"keyword": " ", "value": " "}]}
+    ]
+    parameters.compute.pressure = [{"group": "all", "type": ["thermo_temp"]}]
+    parameters.compute["stress/atom"] = [{"group": "all", "type": ["NULL"]}]
     parameters.control = AttributeDict()
     parameters.control.timestep = 1.0e-05
-    parameters.control.units = 'metal'
+    parameters.control.units = "metal"
     parameters.dump = AttributeDict()
     parameters.dump.dump_rate = 1000
     parameters.fix = AttributeDict()
-    parameters.fix['box/relax'] = [{'group': 'all', 'type':['iso', 0.0, 'vmax', 0.001]}]
+    parameters.fix["box/relax"] = [
+        {"group": "all", "type": ["iso", 0.0, "vmax", 0.001]}
+    ]
     parameters.md = AttributeDict()
     parameters.md.integration = AttributeDict()
     parameters.md.integration.constraints = AttributeDict()
     parameters.md.integration.constraints.iso = [0.0, 0.0, 1000.0]
     parameters.md.integration.constraints.temp = [300, 300, 100]
-    parameters.md.integration.style = 'npt'
+    parameters.md.integration.style = "npt"
     parameters.potential = AttributeDict()
     parameters.structure = AttributeDict()
-    parameters.structure.atom_style = 'atomic'
+    parameters.structure.atom_style = "atomic"
     parameters.thermo = AttributeDict()
     parameters.thermo.printing_rate = 100
     parameters.thermo.thermo_printing = AttributeDict()
@@ -78,26 +85,32 @@ def parameters_minimize() -> AttributeDict:
     """
     parameters = AttributeDict()
     parameters.compute = AttributeDict()
-    parameters.compute['ke/atom'] = [{'group': 'all', 'type':[{'keyword': " ", "value": " "}]}]
-    parameters.compute['pe/atom'] = [{'group': 'all', 'type':[{'keyword': " ", "value": " "}]}]
-    parameters.compute.pressure = [{'group': 'all', 'type':['thermo_temp']}]
-    parameters.compute['stress/atom'] = [{'group': 'all', 'type':['NULL']}]
+    parameters.compute["ke/atom"] = [
+        {"group": "all", "type": [{"keyword": " ", "value": " "}]}
+    ]
+    parameters.compute["pe/atom"] = [
+        {"group": "all", "type": [{"keyword": " ", "value": " "}]}
+    ]
+    parameters.compute.pressure = [{"group": "all", "type": ["thermo_temp"]}]
+    parameters.compute["stress/atom"] = [{"group": "all", "type": ["NULL"]}]
     parameters.control = AttributeDict()
     parameters.control.timestep = 1.0e-05
-    parameters.control.units = 'metal'
+    parameters.control.units = "metal"
     parameters.dump = AttributeDict()
     parameters.dump.dump_rate = 1000
     parameters.fix = AttributeDict()
-    parameters.fix['box/relax'] = [{'group': 'all', 'type':['iso', 0.0, 'vmax', 0.001]}]
+    parameters.fix["box/relax"] = [
+        {"group": "all", "type": ["iso", 0.0, "vmax", 0.001]}
+    ]
     parameters.minimize = AttributeDict()
     parameters.minimize.energy_tolerance = 0.0001
-    parameters.minimize.force_tolerance= 1.0e-05
-    parameters.minimize.max_evaluations= 5000
-    parameters.minimize.max_iterations= 5000
-    parameters.minimize.style= 'cg'
+    parameters.minimize.force_tolerance = 1.0e-05
+    parameters.minimize.max_evaluations = 5000
+    parameters.minimize.max_iterations = 5000
+    parameters.minimize.style = "cg"
     parameters.potential = AttributeDict()
     parameters.structure = AttributeDict()
-    parameters.structure.atom_style = 'atomic'
+    parameters.structure.atom_style = "atomic"
     parameters.thermo = AttributeDict()
     parameters.thermo.printing_rate = 100
     parameters.thermo.thermo_printing = AttributeDict()
